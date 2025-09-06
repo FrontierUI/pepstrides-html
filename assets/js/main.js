6c7de483b72e08e566bed365c167e8682c966ea1
FrontierUI <uifrontier95@gmail.com>
2025-09-06 21:31:20 +0500

    innitial commit

---
+++ assets/js/main.js
@@ -0,0 +1,67 @@
+// Navbar
+
+var toggleOpen = document.getElementById('toggleOpen');
+var toggleClose = document.getElementById('toggleClose');
+var collapseMenu = document.getElementById('collapseMenu');
+
+function handleClick() {
+  if (collapseMenu.style.display === 'block') {
+    collapseMenu.style.display = 'none';
+  } else {
+    collapseMenu.style.display = 'block';
+  }
+}
+
+toggleOpen.addEventListener('click', handleClick);
+toggleClose.addEventListener('click', handleClick);
+
+// Sliders
+
+$('.prodSliderWrap').slick({
+  dots: false,
+  arrows: false,
+  autoplay: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: { slidesToShow: 3, slidesToScroll: 1, dots: false },
+    },
+
+    { breakpoint: 600, settings: { slidesToShow: 1, slidesToScroll: 1 } },
+    { breakpoint: 480, settings: { slidesToShow: 1, slidesToScroll: 1 } },
+  ],
+});
+
+$('.reviewSlider').slick({
+  vertical: true,
+  centerMode: true,
+  centerPadding: '0',
+  slidesToShow: 2,
+  autoplay: true,
+  autoplaySpeed: 2500,
+  arrows: false,
+  dots: false,
+  infinite: true,
+});
+
+// Accordions faq
+
+const items = document.querySelectorAll('.accordion button');
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach((item) => item.addEventListener('click', toggleAccordion));
